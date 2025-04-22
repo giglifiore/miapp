cbe9ca7655aac70ce6c1731492943063a55815eb
giglifiore <49178731+giglifiore@users.noreply.github.com>
2025-04-22 17:09:29 -0400

    Update urls.py

--- miapp/urls.py
+++ miapp/urls.py
@@ -1,5 +1,7 @@
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('home.urls')),
+    path('', include('home.urls')),  # redirige la URL base a la app "home"
+    path('admin/', admin.site.urls),
 ]
