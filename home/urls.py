bb28188a7f7829d7f59edf4da97f3470927be155
giglifiore <49178731+giglifiore@users.noreply.github.com>
2025-04-22 19:10:39 -0400

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from home import views
+from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.test, name='test'),  # Mostrará el mensaje en la página principal
 ]
