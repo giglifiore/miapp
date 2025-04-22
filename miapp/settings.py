d419a9b7e8a2d78c2aa402c2d4e0b697b0f4d96e
giglifiore <49178731+giglifiore@users.noreply.github.com>
2025-04-22 15:52:25 -0400

    Add files via upload

---
+++ miapp/settings.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'clave-secreta-fake-para-desarrollo'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+    'django.contrib.staticfiles',
+    'home',
+]
+
+MIDDLEWARE = [
+    'django.middleware.common.CommonMiddleware',
+]
+
+ROOT_URLCONF = 'miapp.urls'
+
+TEMPLATES = []
+
+WSGI_APPLICATION = 'miapp.wsgi.application'
+
+STATIC_URL = '/static/'
